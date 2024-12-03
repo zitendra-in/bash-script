278182664578503242b773316b930d352d594e3a
root <root@localhost.localdomain>
2024-12-03 13:15:47 +0530

    Adding 2.read_input.sh file

---
+++ 1.Basic/2.read_input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#################################################
+# read
+
+read -p "Enter your first name : " a
+read -p "Enter your last name : " b
+
+echo "your full name is $a $b"
+
