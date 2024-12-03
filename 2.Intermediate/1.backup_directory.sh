60eb5f5213a491a11315d41b2d23bb6058c5bdec
root <root@localhost.localdomain>
2024-12-03 17:55:01 +0530

    update

---
+++ 2.Intermediate/1.backup_directory.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+################### Variables ###################
+
+#SOURCE_DIR="/path/to/source_directory"
+read -p "Please Enter the path to SOURCE_DIR : " SOURCE_DIR
+
+#BACKUP_DIR="/path/to/backup_directory"
+read -p "Please Enter the path to BACKUP_DIR : " BACKUP_DIR
+
+# date stamp in backup directory
+DATE=$(date +"%Y%m%d_%H%M%S")
+BACKUP_FILE="backup_${DATE}.tar.gz"
+
+################### Backup_directory ############
+mkdir -p "$BACKUP_DIR"
+
+
+################### Create backup ###############
+# The archive does not contain the directory it contains only its contents.
+# When you want to archive the directory as a whole
+tar -czvf $BACKUP_DIR/$BACKUP_FILE -C $SOURCE_DIR .
+
+# The archive does contain the directory it contains only its contents.
+# When you want to archive only the contents
+#tar -czvf $BACKUP_DIR/$BACKUP_FILE $SOURCE_DIR
+
+
+################### Verify the task #############
+
+if [ $? -eq 0 ]
+then
+	echo "Backup is successful : ${BACKUP_DIR}/${BACKUP_FILE}"
+else
+	echo "Backup is failed !"
+	exit 1
+fi 
+
